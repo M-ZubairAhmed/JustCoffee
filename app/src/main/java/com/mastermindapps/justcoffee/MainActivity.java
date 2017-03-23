7d64bb0fa29d898fbacc2f9bb5be5ff3eafd904d
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-23 22:08:19 +0530

    code for add and minus

--- app/src/main/java/com/mastermindapps/justcoffee/MainActivity.java
+++ app/src/main/java/com/mastermindapps/justcoffee/MainActivity.java
@@ -2,12 +2,41 @@ package com.mastermindapps.justcoffee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.this.setTitle("Order Coffee");
+
+        final TextView quantityView = (TextView) findViewById(R.id.quantity_var_xml);
+        android.support.design.widget.FloatingActionButton addQuantity = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_add_xml);
+        android.support.design.widget.FloatingActionButton minusQuantity = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_minus_xml);
+
+        quantityView.setText(String.valueOf(quantity));
+
+        addQuantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantity++;
+                quantityView.setText(String.valueOf(quantity));
+            }
+        });
+        minusQuantity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quantity != 0){
+                    quantity--;
+                    quantityView.setText(String.valueOf(quantity));
+                }
+            }
+        });
+
+
     }
 }
