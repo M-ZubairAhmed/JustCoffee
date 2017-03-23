7109279ad3ea87b07059568eeffb2c6bc46eb4fd
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-23 23:23:47 +0530

    (+) disable submit when quantity = 0

--- app/src/main/java/com/mastermindapps/justcoffee/MainActivity.java
+++ app/src/main/java/com/mastermindapps/justcoffee/MainActivity.java
@@ -1,13 +1,19 @@
 package com.mastermindapps.justcoffee;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int quantity = 1;
 
+    private int quantity = 1;
+    TextView submitOrder;
+    FrameLayout submitOrderRoot;
+    int COLOR_BROWN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,9 +21,14 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.this.setTitle("Order Coffee");
 
         final TextView quantityView = (TextView) findViewById(R.id.quantity_var_xml);
-        TextView submitOrder = (TextView) findViewById(R.id.submit_order_xml);
+
+        submitOrder = (TextView) findViewById(R.id.submit_order_xml);
+        submitOrderRoot = (FrameLayout) findViewById(R.id.submit_order_root_xml);
+
         android.support.design.widget.FloatingActionButton addQuantity = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_add_xml);
         android.support.design.widget.FloatingActionButton minusQuantity = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_remove_xml);
+
+        COLOR_BROWN = ContextCompat.getColor(this, R.color.colorPrimary);
 
         quantityView.setText(String.valueOf(quantity));
 
@@ -26,18 +37,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 quantity++;
                 quantityView.setText(String.valueOf(quantity));
+                submitStatus();
             }
         });
+
         minusQuantity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (quantity != 0){
                     quantity--;
                     quantityView.setText(String.valueOf(quantity));
+                    submitStatus();
                 }
             }
         });
 
+    }
 
+    protected void submitStatus() {
+        if (quantity == 0) {
+            submitOrder.setClickable(false);
+            submitOrderRoot.setBackgroundColor(Color.LTGRAY);
+        } else {
+            submitOrder.setClickable(true);
+            submitOrderRoot.setBackgroundColor(COLOR_BROWN);
+        }
     }
 }
