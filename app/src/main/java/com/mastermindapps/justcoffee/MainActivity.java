353391e2c8cbb38c2fb30e0a371fafacc52410ad
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-24 11:58:15 +0530

    minor changes in variables names

--- app/src/main/java/com/mastermindapps/justcoffee/MainActivity.java
+++ app/src/main/java/com/mastermindapps/justcoffee/MainActivity.java
@@ -4,63 +4,78 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private int quantity = 1;
-    TextView submitOrder;
+    TextView submitOrderButton;
     FrameLayout submitOrderRoot;
+    TextView quantityViewField;
     int COLOR_BROWN;
+    private int quantityValue = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MainActivity.this.setTitle("Order Coffee");
 
-        final TextView quantityView = (TextView) findViewById(R.id.quantity_var_xml);
+        quantityViewField = (TextView) findViewById(R.id.quantity_var_xml);
 
-        submitOrder = (TextView) findViewById(R.id.submit_order_xml);
+        submitOrderButton = (TextView) findViewById(R.id.submit_order_xml);
         submitOrderRoot = (FrameLayout) findViewById(R.id.submit_order_root_xml);
 
-        android.support.design.widget.FloatingActionButton addQuantity = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_add_xml);
-        android.support.design.widget.FloatingActionButton minusQuantity = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_remove_xml);
+        android.support.design.widget.FloatingActionButton addQuantityButton = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_add_xml);
+        android.support.design.widget.FloatingActionButton minusQuantityButton = (android.support.design.widget.FloatingActionButton) findViewById(R.id.quantity_remove_xml);
 
-        COLOR_BROWN = ContextCompat.getColor(this, R.color.colorPrimary);
+        COLOR_BROWN = hexColorToInt(R.color.colorPrimary);
 
-        quantityView.setText(String.valueOf(quantity));
+        displayQuantityMethod();
 
-        addQuantity.setOnClickListener(new View.OnClickListener() {
+        addQuantityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                quantity++;
-                quantityView.setText(String.valueOf(quantity));
-                submitStatus();
+                quantityValue++;
+                displayQuantityMethod();
+                submitSwitchMethod();
             }
         });
 
-        minusQuantity.setOnClickListener(new View.OnClickListener() {
+        minusQuantityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (quantity != 0){
-                    quantity--;
-                    quantityView.setText(String.valueOf(quantity));
-                    submitStatus();
+                if (quantityValue != 0) {
+                    quantityValue--;
+                    displayQuantityMethod();
+                    submitSwitchMethod();
                 }
             }
         });
 
+
+
     }
 
-    protected void submitStatus() {
-        if (quantity == 0) {
-            submitOrder.setClickable(false);
+    protected void submitSwitchMethod() {
+        if (quantityValue == 0) {
+            submitOrderButton.setClickable(false);
             submitOrderRoot.setBackgroundColor(Color.LTGRAY);
         } else {
-            submitOrder.setClickable(true);
+            submitOrderButton.setClickable(true);
             submitOrderRoot.setBackgroundColor(COLOR_BROWN);
         }
+    }
+
+    protected void displayQuantityMethod() {
+        quantityViewField.setText(String.valueOf(quantityValue));
+    }
+
+    protected int hexColorToInt(int colorResource){
+        return ContextCompat.getColor(this,colorResource);
     }
 }
